5aacffbeb9903bd35c4e7b5dab79beddebb69de5
Airat Galiamov <33791465+DuzaBF@users.noreply.github.com>
2023-03-03 20:57:07 +0100

    Add complex conductivity

--- src/tissue_data.py
+++ src/tissue_data.py
@@ -1,6 +1,7 @@
 import csv
 import numpy
 import argparse
+import scipy.constants
 
 
 class TissueData(object):
@@ -13,9 +14,9 @@ class TissueData(object):
         with open(filename) as file:
             reader = csv.reader(file)
             for row in reader:
-                self.freq.append(row[0])
-                self.sigma.append(row[1])
-                self.eps.append(row[2])
+                self.freq.append(float(row[0]))
+                self.sigma.append(float(row[1]))
+                self.eps.append(float(row[2]))
         self.freq = numpy.array(self.freq)
         self.sigma = numpy.array(self.sigma)
         self.eps = numpy.array(self.eps)
@@ -31,6 +32,10 @@ class TissueData(object):
     def __repr__(self) -> str:
         return str(self)
 
+    @property
+    def complex_sigma(self):
+        return self.sigma + numpy.multiply(self.freq, self.eps * scipy.constants.epsilon_0) * 1j * 2*scipy.constants.pi  
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
@@ -41,3 +46,7 @@ if __name__ == "__main__":
     td = TissueData(args.filename)
 
     print(td)
+    str = "{:>{width}} | {:>{width}}\n".format("freq [Hz]","sigma [S/m]",width=10)
+    for i, f in enumerate(td.freq):
+        str = str + "{:>{width}} | {:>{width}}\n".format(f,"{:.3f}".format(td.complex_sigma[i]),width=10)
+    print(str)
