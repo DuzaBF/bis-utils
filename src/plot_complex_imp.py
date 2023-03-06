b730d6f58915e04712d15dc31ed1797042062329
Airat Galiamov <33791465+DuzaBF@users.noreply.github.com>
2023-03-06 21:24:14 +0100

    Add plotting complex conductivity

--- src/plot_complex_imp.py
+++ src/plot_complex_imp.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy
 
 
-def plot_imp_data(data, freq=None, xaxis_name="", yaxis_name="", title=0):
+def make_figure():
     figure = plt.figure()
     axes = figure.add_axes(rect=[0.1, 0.1, 0.8, 0.8])
     y_pos = 0
@@ -17,25 +17,48 @@ def plot_imp_data(data, freq=None, xaxis_name="", yaxis_name="", title=0):
     # Show ticks in the left and lower axes only
     axes.xaxis.set_ticks_position('bottom')
     axes.yaxis.set_ticks_position('left')
-    axes.scatter(data.real, data.imag, facecolor="black")
     axes.plot(1, axes.spines['bottom'].get_position()[1], ">k",
                         transform=axes.get_yaxis_transform(), clip_on=False)
     axes.plot(axes.spines['left'].get_position()[1], 1, "^k",
                         transform=axes.get_xaxis_transform(), clip_on=False)
+    return figure
 
-    plt.show()
-
+def plot_imp_data(data=None, figure=None, freq=None, xaxis_name="", yaxis_name="", title=0):
+    if figure is None:
+        figure = plt.figure()
+    figure.axes[0].plot(data.real, data.imag)
 
 if __name__ == "__main__":
     import argparse
     import tissue_data
+    import electrical_sounding as es
     parser = argparse.ArgumentParser(
                     prog = 'Plot complex impedance')
-    parser.add_argument('filename') 
+    parser.add_argument('-f', '--fat', required=False) 
+    parser.add_argument('-m', '--muscle', required=False) 
     args = parser.parse_args()
 
-    td = tissue_data.TissueData(args.filename)
+    td_fat = tissue_data.TissueDataComplex(args.fat)
+    td_muscle = tissue_data.TissueDataComplex(args.muscle)
 
-    print(td.complex_sigma)
+    L = 30 * 10**(-3) # [m]
+    s = 10 * 10**(-3) # [m]
+    d_1 = 10 * 10**(-3) # [m]
 
-    plot_imp_data(td.complex_sigma)
+    a = numpy.array(list(zip(td_fat.complex_sigma, td_muscle.complex_sigma)))
+    print(a)
+    b = list(map(lambda x: es.apparent_conductivity(x[0], x[1], L, s, d_1), zip(td_fat.complex_sigma, td_muscle.complex_sigma)))
+    print(b)
+
+    sigma_apparent = numpy.array(list(map(lambda x: es.apparent_conductivity(x[0], x[1], L, s, d_1), zip(td_fat.complex_sigma, td_muscle.complex_sigma))))
+    print(td_fat)
+    print(td_muscle)
+    print(sigma_apparent)
+
+    figure = make_figure()
+
+    plot_imp_data(data=td_fat.complex_sigma, figure=figure)
+    plot_imp_data(data=td_muscle.complex_sigma, figure=figure)
+    plot_imp_data(data=sigma_apparent, figure=figure)
+
+    plt.show()
