d47767d484003558d3257123882083e2dc5882d8
Airat Galiamov <33791465+DuzaBF@users.noreply.github.com>
2023-03-12 14:46:11 +0100

    Update on cell complex conductivity

--- src/mixture_models.py
+++ src/mixture_models.py
@@ -1,5 +1,22 @@
+import scipy.constants as sc
+
 def hanai_equation(sigma, sigma_m, sigma_p):
-    return (((sigma - sigma_p) / (sigma_m -sigma_p)) * (((sigma_m) / (sigma)) ** (1/3)))
+    return (((sigma - sigma_p) / (sigma_m - sigma_p)) * (((sigma_m) / (sigma)) ** (1/3)))
+
+def cell_sigma(sigma_icf, C_m, t, freq):
+    return sigma_icf * (1 + ((2*sigma_icf) / (1j * 2 * sc.pi * freq * C_m * t))) ** (-1)
 
 if __name__ == "__main__":
-    pass
+    import parameters
+    import tissue_data as td
+    freq = 100000 # [Hz]
+    csigma_icf = td.TissueDataComplex.to_complex(freq, parameters.sigma_icf, parameters.eps_icf)
+    csigma_ecf = td.TissueDataComplex.to_complex(freq, parameters.sigma_ecf, parameters.eps_ecf)
+    csigma_cell = cell_sigma(csigma_icf, parameters.C_m, parameters.t, freq)
+    csigma_fat = td.TissueDataComplex.to_complex(freq, parameters.sigma_fat, parameters.eps_fat)
+    vol_conc = 1 - hanai_equation(csigma_fat, csigma_ecf, csigma_cell)
+
+    print("{:>32} {:>6}: {:>5.4f} S/m at {:.0f} kHz".format("Complex conductivity of", "an ICF", csigma_icf, freq/1000))
+    print("{:>32} {:>6}: {:>5.4f} S/m at {:.0f} kHz".format("Complex conductivity of", "a cell", csigma_cell, freq/1000))
+    print("{:>32} {:>6}: {:>5.4f} S/m at {:.0f} kHz".format("Complex conductivity of", "an ECF", csigma_ecf, freq/1000))
+    print("{:>40} {:>5.4f}".format("Volume concentration of cells in fat:", vol_conc))
--- src/parameters.py
+++ src/parameters.py
@@ -4,20 +4,26 @@ L - distance between current electrodes
 s - distance between measurement electrodes
 d_1 - thickness of fat layer
 sigma_fat - conductivity of fat at 100 kHz
+eps_fat - dielectric constant of fat at 100 kHz
 sigma_muscle - conductivity of muscle at 100 kHz
+eps_muscle - dielectric constant of muscle at 100 kHz
 eps_icf - dielectric constant of the intracellular fluid (cytoplasm)
 sigma_icf - conductivity of the intracellular fluid (cytoplasm)
+eps_ecf - dielectric constant of the extracellular fluid
 sigma_ecf - conductivity of the extracellular fluid
-C_m - cell membrane capacitance per unit length
+C_m - cell membrane capacitance per unit area
 t - size of the cell
 '''
 L = 60 * 10**(-3) # [m]
 s = 40 * 10**(-3) # [m]
 d_1 = 5 * 10**(-3) # [m]
 sigma_fat = 0.02 # [S/m]
+eps_fat = 92.885
 sigma_muscle = 0.34 # [S/m]
+eps_muscle = 8089.2
 eps_icf = 80
 sigma_icf = 0.3 # [S/m]
-sigma_exf = 1.2 # [S/m]
+eps_ecf = 80
+sigma_ecf = 1.2 # [S/m]
 C_m = 0.01 # [F/m^2]
 t = 30 * 10**(-6) # [m]
