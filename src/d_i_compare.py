a2c6c447835a8a5bb46b582750e33508f20877f4
Airat Galiamov <33791465+DuzaBF@users.noreply.github.com>
2024-05-10 21:40:51 +0200

    Add skin effect depth and wavelength

--- src/d_i_compare.py
+++ src/d_i_compare.py
@@ -1,4 +1,5 @@
 import scipy.constants
+import numpy
 
 """
 This module contains a function, which compares conductivity and displacement currents for a given media and frequency.
@@ -9,6 +10,22 @@ L. D. Landau, E. M. Lifšic, L. P. Pitaevskij, and L. D. Landau, “The field eq
 def calc_ratio(eps, sigma, omega):
     return (eps * scipy.constants.epsilon_0 * omega) / (sigma)
 
+"""
+Skin depth
+The magnetic permeability of a biological tissue is close to that of the free space μ0 and assumed to be the constant.
+The quasi-static approximation is valid for bodies of moderate conductivity, 
+if the dimentions of interest are much smaller than the electromagnetic wavelength and the skin depth. 
+The skin depth characterizes the outer layer of a conductor where most
+electric current flows; it is is most pronounced with very conductive materials in which significant eddy currents flow
+Reference:
+D. Holder and A. Adler, Eds., Electrical impedance tomography: methods, history and applications, Second. Boca Raton: CRC Press, 2022.
+"""
+def skin_depth(freq, mu):
+    return 1 / numpy.sqrt(scipy.constants.pi * freq * scipy.constants.mu_0 * mu * sigma)
+
+def wavelength(freq, eps, mu):
+    c = 1 / numpy.sqrt(eps * scipy.constants.epsilon_0  * scipy.constants.mu_0 * mu)
+    return c / freq
 
 if __name__ == "__main__":
     """
@@ -26,9 +43,10 @@ if __name__ == "__main__":
         10000,
         3000,
     ]
-    freqs = [100 * 1000] * 4
+    mus = [1, 1, 1, 1]
+    freqs = [100 * 1000, 100 * 1000, 100 * 1000, 100 * 1000]
     names = ["Adipose", "Muscle (Mixed)", "Muscle (Along)", "Muscle (Across)"]
-    table_row_names = ("Name", "f [kHz]", "sigma [S/m]", "eps", "Id/Ic")
+    table_row_names = ("Name", "f [kHz]", "sigma [S/m]", "eps", "Id/Ic", "delta, m", "Wavelength, m")
     table_row_template = ""
     for i in range(len(table_row_names)):
         table_row_template += "{:>15s} | "
@@ -39,5 +57,7 @@ if __name__ == "__main__":
         eps = epss[i]
         freq = freqs[i]
         ratio = calc_ratio(eps, sigma, 2 * scipy.constants.pi * freq)
-        values = ["{:5.5f}".format(x) for x in [freq / 1000, sigma, eps, ratio]]
+        delta = skin_depth(freq, mus[i])
+        wl_lambda = wavelength(freq, eps, mus[i])
+        values = ["{:5.5f}".format(x) for x in [freq / 1000, sigma, eps, ratio, delta, wl_lambda]]
         print(table_row_template.format(name, *values))
